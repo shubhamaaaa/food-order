30aaeaf4e46c318fdbc95244e9cc27e9777f86f1
shubhamaaaa <130844125+shubhamaaaa@users.noreply.github.com>
2024-08-11 09:22:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import orderRouter from './routes/orderRoute.js';
 
 //App Config
 const app=express();
-const port=3000;
+const port=process.env.PORT || 3000;
 
 //Middleware
 app.use(express.json())
